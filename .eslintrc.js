04657afa727688edf663152af1857b1b3cdda35b
amusameh <abdalsamad.y.m@gmail.com>
2019-02-26 21:48:10 +0200

    chore; initial setup + eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "node": true,
+  },
+  "rules": {
+    // disables the windows/unix linebreak checks.
+    "linebreak-style": 0,
+    "linebreak-style": [0, "error", "windows"]
+  }
+};
